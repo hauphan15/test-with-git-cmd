293f816fdef8b9ea084d5c8ba1e23729e04741ad
hauphan15 <49446469+hauphan15@users.noreply.github.com>
2021-07-31 16:02:05 +0700

    cm

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express, { json, urlencoded } from 'express'
-import morgan from 'morgan'
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express()
-app.use(json())
+app.use(express.json())
 app.use(morgan('dev'))
 app.use(urlencoded({
     extended: true
