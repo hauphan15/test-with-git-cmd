e37de407c922865b22fcb85970fd7fc927ccace0
hauphan15 <49446469+hauphan15@users.noreply.github.com>
2021-07-31 16:03:55 +0700

    cm

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(urlencoded({
+app.use(express.urlencoded({
     extended: true
 }))
 
