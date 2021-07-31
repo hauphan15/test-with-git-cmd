6c6438e2d3b0302d43df60b48b5cee45d1937116
hauphan15 <49446469+hauphan15@users.noreply.github.com>
2021-07-31 13:11:28 +0700

    cm

---
+++ app.js
@@ -0,0 +1,28 @@
+import express, { json, urlencoded } from 'express'
+import morgan from 'morgan'
+
+const app = express()
+app.use(json())
+app.use(morgan('dev'))
+app.use(urlencoded({
+    extended: true
+}))
+
+app.get('/', function(req, res) {
+    res.send('Hello from Mrhauphan !!');
+})
+
+app.get('/employee-list', function(req, res) {
+    const list = [
+        {id: 0, name: 'phan van hau'},
+        {id: 1, name: 'phan van nhon'},
+        {id: 2, name: 'nguyen thuy kieu'},
+        {id: 3, name: 'phan van tho'},
+    ]
+    res.send(list);
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`API is running on port ${PORT}`);
+})
